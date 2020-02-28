6d7bfd7d3d6a94e0c9a23ef0dde0a2dba755e3eb
testrod <33133548+testrod@users.noreply.github.com>
2020-02-28 15:51:58 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+smtp = {'host': 'YOUR SMTP IP',
+	'port': 'YOUR SMTP PORT'
+	}
+
+mail = {'from': 'bla@bla.com',
+	'to': ['player1@bla.com','player2@afip.gob.ar'],
+	'subject': 'netstatAlert [UDP STATS]'
+	}
+
+use_anonymous = True
